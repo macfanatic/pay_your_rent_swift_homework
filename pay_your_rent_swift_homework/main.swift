04d93729be2b9936cd36409ec83b62e28f8b7f08
Matt Millsaps-Brewer <matt@madebylotus.com>
2017-03-21 22:05:25 -0400

    Initial Commit

---
+++ pay_your_rent_swift_homework/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  pay_your_rent_swift_homework
+//
+//  Created by Matthew Millsaps-Brewer on 3/21/17.
+//  Copyright © 2017 Matthew Millsaps-Brewer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
