b71c876ffb5c8f020e09eea590c70c601066ec17
Matt Millsaps-Brewer <matt@madebylotus.com>
2017-03-21 23:45:14 -0400

    Makes a Lease sortable

--- pay_your_rent_swift_homework/Lease.swift
+++ pay_your_rent_swift_homework/Lease.swift
@@ -8,7 +8,11 @@
 
 import Foundation
 
-class Lease {
+enum LeasingError : Error {
+    case invalidFormat
+}
+
+class Lease : Comparable {
     var unit: String
     var name: String
     var raw: String
@@ -16,12 +20,23 @@ class Lease {
     fileprivate var numericUnit: Int?
     fileprivate var alphaUnit: String?
     
-    init(unit: String, name: String, raw: String) {
+    init(unit: String, name: String, raw: String) throws {
         self.unit = unit
         self.name = name
         self.raw = raw
         
-        parseUnit()
+        try parseUnit()
+    }
+    
+    static func ==(left:Lease, right:Lease) -> Bool {
+        return (left.numericUnit!, left.alphaUnit!) == (right.numericUnit!, right.alphaUnit!)
+    }
+    
+    static func <(left:Lease, right:Lease) -> Bool {
+        if left.numericUnit != right.numericUnit {
+            return left.numericUnit! < right.numericUnit!
+        }
+        return left.alphaUnit! < right.alphaUnit!
     }
     
     static func parse(_ string: String) -> Lease? {
@@ -31,7 +46,7 @@ class Lease {
                 let unit = (string as NSString).substring(with: match.rangeAt(1))
                 let name = (string as NSString).substring(with: match.rangeAt(2))
                 
-                return Lease(unit: unit, name: name, raw: string)
+                return try Lease(unit: unit, name: name, raw: string)
             }
             
             return nil
@@ -40,16 +55,18 @@ class Lease {
         }
     }
     
-    private func parseUnit() {
+    private func parseUnit() throws {
         var regex = try! NSRegularExpression(pattern: "([a-zA-Z]+)(\\d*)", options: .caseInsensitive)
         if let match = regex.firstMatch(in: self.unit, options: .anchored, range: NSRange(location: 0, length: self.unit.utf16.count)) {
             self.alphaUnit = (self.unit as NSString).substring(with: match.rangeAt(1))
-            self.numericUnit = Int((self.unit as NSString).substring(with: match.rangeAt(2)))
+            self.numericUnit = Int((self.unit as NSString).substring(with: match.rangeAt(2)))!
         } else {
             regex = try! NSRegularExpression(pattern: "(\\d+)([a-zA-Z]*)", options: .caseInsensitive)
             if let match = regex.firstMatch(in: self.unit, options: .anchored, range: NSRange(location: 0, length: self.unit.utf16.count)) {
-                self.numericUnit = Int((self.unit as NSString).substring(with: match.rangeAt(1)))
+                self.numericUnit = Int((self.unit as NSString).substring(with: match.rangeAt(1)))!
                 self.alphaUnit = (self.unit as NSString).substring(with: match.rangeAt(2))
+            } else {
+                throw LeasingError.invalidFormat
             }
         }
     }
